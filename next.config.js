e6aab4bd96a955921e046a0d59255eddd874c5fd
parkercon <61677313+parkercon@users.noreply.github.com>
2024-01-24 14:39:14 -0800

    enabled static export
    When running next build, Next.js generates an HTML file per route. By breaking a strict SPA into individual HTML files, Next.js can avoid loading unnecessary JavaScript code on the client-side, reducing the bundle size and enabling faster page loads.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    output: 'export',
     webpack: (config, { isServer }) => {
         // Add the file-loader rule for PDF files
         config.module.rules.push({
